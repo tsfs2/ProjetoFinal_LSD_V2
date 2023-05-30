e8864f57ab98f78a95408e4c481439fd0701b8df
e-Paul0 <130754722+e-Paul0@users.noreply.github.com>
2023-05-30 15:48:20 +0100

    02.divFreq.vhd

---
+++ FINAL/Parte1_RelogioAcerto/Bloco0_PulseGen/02.divFreq.vhd
@@ -0,0 +1,32 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
+entity DivFreq is
+	generic(contagem 	: natural := 20);
+	port(clkIn	: in  std_logic;
+		  clkOut	: out std_logic);
+end DivFreq;
+
+architecture compDivFreq of DivFreq is
+
+	signal s_divCounter : natural;
+
+begin
+	assert(contagem >= 2);
+	
+	process(clkIn)
+	begin
+		if (rising_edge(clkIn)) then
+			if (s_divCounter = contagem - 1) then
+				clkOut		 <= '0';
+				s_divCounter <= 0;
+			else
+				if (s_divCounter = (contagem / 2 - 1)) then
+					clkOut	 <= '1';
+				end if;
+				s_divCounter <= s_divCounter + 1;
+			end if;
+		end if;
+	end process;
+end compDivFreq;
