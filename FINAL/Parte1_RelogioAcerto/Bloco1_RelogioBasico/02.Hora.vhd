4a59314ed5ca1d4504c10823107d17f114579c0f
e-Paul0 <130754722+e-Paul0@users.noreply.github.com>
2023-05-31 15:14:49 +0100

    02.Hora.vhd

--- FINAL/Parte1_RelogioAcerto/Bloco1_RelogioBasico/02.Hora.vhd
+++ FINAL/Parte1_RelogioAcerto/Bloco1_RelogioBasico/02.Hora.vhd
@@ -6,7 +6,7 @@ entity Hora is
 
 	generic (contMax: integer :=23);
 	port (clk 			: in std_logic;
-			enL	: in std_logic;
+			enL			: in std_logic;
 			upD			: in std_logic;
 			horaOut		: out std_logic_vector (4 downto 0));
 end Hora;
@@ -15,6 +15,8 @@ architecture compHora of Hora is
 signal s_cont : integer range 0 to 23;
 begin
 
+	assert(s_cont <= 23);
+
 	process (clk)
 	begin
 		
@@ -22,7 +24,7 @@ begin
 			if (enL = '1') then
 				if (upD = '0') then
 					s_cont <= s_cont -1;
-				elsif (s_cont = contMax) then
+				elsif (s_cont >= contMax) then
 					s_cont <= 0;
 				else
 					s_cont <= s_cont + 1;
